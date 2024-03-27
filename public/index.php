ddc74e6d51f4cbbf4e9fbed993010d696ea050de
OlegSofonov <olesof@mail.ru>
2024-03-27 16:45:37 +0500

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,23 +2,40 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use SalesRender\Plugin\Instance\Chat\Components\MainSmsApi;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('localhost/index.php/special/send', function (Request $request, Response $response, $args) {
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->post('/special/send', function (Request $request, Response $response, $args) {
+    $data = json_decode($request->getBody()->getContents(), true);
+    $mainSmailApi = new MainSmsApi($data['apikey']);
+    $mainSmsResponseBodyArray = $mainSmailApi->sendEmail(
+        $data['sender'],
+        $data['name'],
+        $data['subject'],
+        $data['recipient'],
+        $data['text']
+    );
+
+    var_dump($mainSmsResponseBodyArray);
+    if (!array_key_exists('errors', $mainSmsResponseBodyArray)) {
+        return $response->withStatus(202);
+    } else {
+        return $response->withStatus(405);
+    }
+
+});
+
+$app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
-
-$app->post('/special/send', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Other Hello world!");
-    //$response->getBody()->getContents();
-    var_dump($response);
-    return $response;
-});
-
 
 $app->run();
